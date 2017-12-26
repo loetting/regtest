d78d06a2dd098ff62880db657ea6fa2d7f08690a
Lilli Oetting <loetting@gofundme.com>
2017-12-26 15:11:42 -0600

    update Dandelion class to return set of results

--- RegulationNLP/Dandelion.py
+++ RegulationNLP/Dandelion.py
@@ -12,6 +12,7 @@ class Dandelion:
 
 
     def getEntities(self, text):
+        keywords = set()
         min_index = 0
         max_index = self.CHAR_LIMIT if len(text) > self.CHAR_LIMIT else len(text)
         while max_index <= len(text):
@@ -23,11 +24,11 @@ class Dandelion:
                     'min_confidence': self.MIN_CONFIDENCE
                 }
             response = requests.get(self.URL, params=params)
-            print "Keywords " + str(min_index) + ":" + str(max_index)
             for annotation in response.json()['topEntities']:
-                print annotation['uri']
+                keywords.add(annotation['uri'])
 
             min_index = max_index
             if (max_index == len(text)):
                 break
             max_index = max_index + self.CHAR_LIMIT if max_index + self.CHAR_LIMIT <= len(text) else len(text)
+        return keywords
